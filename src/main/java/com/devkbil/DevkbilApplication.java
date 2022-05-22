1251816449e2496c923f541fca333ffaf7844932
kbil <linuxint@gmail.com>
2022-05-22 12:55:57 +0900

    package clean

--- src/main/java/com/devkbil/DevkbilApplication.java
+++ src/main/java/com/devkbil/DevkbilApplication.java
@@ -1,17 +1,8 @@
 package com.devkbil;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.devkbil.*")
