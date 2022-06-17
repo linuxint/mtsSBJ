dc283870bd33248a021bf54cc2b2ecb7a0f7752b
linuxint <linuxint@gmail.com>
2022-06-17 10:26:48 +0900

    tab remove

--- src/main/resources/static/js/mts.js
+++ src/main/resources/static/js/mts.js
@@ -1,26 +1,26 @@
-function chkInputValue(id, msg){
-	if ( $.trim($(id).val()) == "") {
-		alert(msg+"을(를) 입력해주세요.");
-		$(id).focus();
-		return false;
-	}
-	return true;
+function chkInputValue(id, msg) {
+    if ($.trim($(id).val()) == "") {
+        alert(msg + "을(를) 입력해주세요.");
+        $(id).focus();
+        return false;
+    }
+    return true;
 }
 
-function fn_moveToURL(url, msg){
-	if (msg) {
-		if (!confirm( msg + " 하시겠습니까??")) return;
-	}
-	location.href=url;
+function fn_moveToURL(url, msg) {
+    if (msg) {
+        if (!confirm(msg + " 하시겠습니까??")) return;
+    }
+    location.href = url;
 }
 
-function fn_moveToURLbyForm(formid, url, msg){
-	if (msg) {
-		if (!confirm( msg + " 하시겠습니까??")) return;
-	}
-	var form = document.getElementById(formid);
-	form.action=url;
-	form.submit();
+function fn_moveToURLbyForm(formid, url, msg) {
+    if (msg) {
+        if (!confirm(msg + " 하시겠습니까??")) return;
+    }
+    var form = document.getElementById(formid);
+    form.action = url;
+    form.submit();
 }
 
 function html2Text(str) {
