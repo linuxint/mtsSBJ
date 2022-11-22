7e0102cdaaf6b20d11c425180571ae7ff9e3ed6f
linuxint <linuxint@gmail.com>
2022-11-22 14:33:52 +0900

    test module

--- src/test/java/com/devkbil/mtssbj/CurrentTimeMillisTest.java
+++ src/test/java/com/devkbil/mtssbj/CurrentTimeMillisTest.java
@@ -1,5 +1,8 @@
 package com.devkbil.mtssbj;
 
+import io.swagger.models.auth.In;
+
+import java.util.Arrays;
 import java.util.Random;
 
 public class CurrentTimeMillisTest {
@@ -10,11 +13,18 @@ public class CurrentTimeMillisTest {
     
         System.out.println("0부터 N까지 무작위 랜덤 숫자 : " + random.nextInt(100));
         System.out.println("무작위 Int 값 : " + random.nextInt());
+        System.out.println("무작위 Int 값 : " + random.nextInt(Integer.MAX_VALUE));
         System.out.println("무작위 Long 값 : " + random.nextLong());
         System.out.println("무작위 Double 값 : " + random.nextDouble());
         System.out.println("무작위 Float 값 : " + random.nextFloat());
         System.out.println("무작위 정규 분포의 랜덤 값 : " + random.nextGaussian());
         System.out.println("true, false : " + random.nextBoolean());
-
+        
+        String[] result = new String[2];
+        result[0] = "1";
+        result[1] = "2";
+        System.out.println(Arrays.toString(result));
+        
+        String filename = "C:\\dev_x64\\apache-tomcat-8.5.83\\conf";
     }
 }
